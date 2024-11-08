bb654368b7b502bb3df8549ab4cbd512a277383e
bdshakhawat <mdshakwat.hossain@gmail.com>
2024-11-08 19:20:32 +0600

    problem-8 solved

--- src/problem_8.ts
+++ src/problem_8.ts
@@ -1,5 +1,33 @@
 {
+// 
+/*
+Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+Sample Input:
+const person = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(validateKeys(person, ["name", "age"]));
+
+Sample Output:
+true;
+
+*/
+
+// Solution of problem 8:
+
+type KeysOf<T> = { [K in keyof T]: K };
+
+function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+    return keys.every((key) => key in obj);
+}
+
+const person = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(validateKeys(person, ["name", "age"]));
 
 
 
+
+
+
+
+// 
 }
